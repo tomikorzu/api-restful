297cfa8a85744f3deaf3aa795e90399ba1264edd
tomikorzu <tomykorzu@icloud.com>
2024-10-29 12:22:20 -0300

    feat(logger.js / index.js): add logger functions to validate authorization / add the get and post methods to test

--- api-restful/index.js
+++ api-restful/index.js
@@ -2,16 +2,13 @@ console.log("API RESTful");
 
 import express from "express";
 
-import { logginMiddleware } from "./logger.js";
+import { logger, authenticate, validateUser } from "./logger.js";
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.json({ message: "Hello World!" });
-});
+app.use(logger);
 
 let users = [
   { id: 1, name: "Tomas", email: "tomykorzu@icloud.com" },
@@ -19,11 +16,11 @@ let users = [
   { id: 3, name: "Silvana", email: "silvana@icloud.com" },
 ];
 
-app.get("/users", (req, res) => {
+app.get("/users", authenticate, (req, res) => {
   res.json(users);
 });
 
-app.get("/users/:id", (req, res) => {
+app.get("/users/:id", authenticate, (req, res) => {
   const id = req.params.id;
   const user = users.find((user) => user.id === parseInt(id));
   if (user) {
@@ -44,7 +41,7 @@ app.post("/users", (req, res) => {
   res.status(201).json(newUser);
 });
 
-app.patch("/users/:id", (req, res) => {
+app.patch("/users/:id", authenticate, (req, res) => {
   const id = req.params.id;
   const user = users.find((user) => user.id === parseInt(id));
   if (user) {
@@ -59,20 +56,29 @@ app.patch("/users/:id", (req, res) => {
   }
 });
 
-app.delete("/users/:id", (req, res) => {
+app.delete("/users/:id", authenticate, (req, res) => {
   const id = req.params.id;
   users = users.filter((user) => user.id !== parseInt(id));
   res.status(200).json({ message: `User ${id} deleted` });
 });
 
-app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message });
+app.post("/register", validateUser, (req, res) => {
+  const newUser = {
+    id: users.length + 1,
+    name: req.body.username,
+    email: req.body.email,
+  };
+  users.push(newUser);
+  res.status(201).json({ message: "User register succesful", user: newUser });
 });
 
-app.use(logginMiddleware);
+app.get("/protected", authenticate, (req, res) => {
+  res.json({ message: "Access allow" });
+});
 
-app.post("/info", (req, res) => {
-  res.json(req.body);
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send("Error in the server");
 });
 
 app.listen(port, () => {
--- api-restful/logger.js
+++ api-restful/logger.js
@@ -1,4 +1,22 @@
-export const logginMiddleware = (req, res, next) => {
-  console.log(`${req.method} - ${req.url}`);
+export const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
   next();
+};
+
+export const authenticate = (req, res, next) => {
+  const token = req.headers["authorization"];
+  if (token && token === "Bearer token123") {
+    next();
+  } else {
+    res.status(401).json({ message: "Not authorized" });
+  }
+};
+
+export const validateUser = (req, res, next) => {
+  const { username, password } = req.body;
+  if (username && password) {
+    next();
+  } else {
+    res.status(400).json({ message: "There are not all data" });
+  }
 };
