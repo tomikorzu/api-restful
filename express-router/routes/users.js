ad26afef7e701d9eac63b6af6a267387c14bdb5a
tomikorzu <tomykorzu@icloud.com>
2024-10-30 12:43:11 -0300

    feat(routes/users.js): add getUsersById router

--- express-router/routes/users.js
+++ express-router/routes/users.js
@@ -9,7 +9,21 @@ router.get("/", (req, res) => {
     if (err) {
       return res.status(500).json({ message: err });
     }
-    res.status(200).json(rows)
+    res.status(200).json(rows);
+  });
+});
+
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  db.get(`SELECT * from users where id = ?`, [id], (err, row) => {
+    if (err) {
+      res.status(500).json({ message: err });
+    }
+    if (row) {
+      res.status(200).json(row);
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
   });
 });
 
