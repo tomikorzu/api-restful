491531b3c05085d1e0a484f7c2057019b2b984be
tomikorzu <tomykorzu@icloud.com>
2024-10-30 12:34:13 -0300

    feat(users.js): add all database functions

--- express-router/config/users.js
+++ express-router/config/users.js
@@ -10,5 +10,32 @@ db.serialize(() => {
             email TEXT NOT NULL UNIQUE
         )
         `);
+
+  db.get(`SELECT COUNT(*) AS count FROM users`, (err, row) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    if (row.count === 0) {
+      const insertValues = db.prepare(
+        `INSERT INTO users (name, email) VALUES (?, ?)`
+      );
+
+      insertValues.run("Tomas", "tomykorzu@icloud.com");
+      insertValues.run("Carlos", "carlos@icloud.com");
+      insertValues.run("Silvana", "silvana@icloud.com");
+
+      insertValues.finalize((err) => {
+        if (err) {
+          console.error("Error inserting users:", err);
+        } else {
+          console.log("Users added successfully");
+        }
+      });
+    } else {
+      console.log("The table users already has content");
+    }
+  });
 });
 
+export default db;
