cc54de6e13f6b51992e203c0629073232e3aeb48
tomikorzu <tomykorzu@icloud.com>
2024-10-30 12:53:24 -0300

    feat(routes/users.js): add patch method to update users

--- express-router/routes/users.js
+++ express-router/routes/users.js
@@ -35,15 +35,35 @@ router.post("/", authenticate, (req, res) => {
       if (err) {
         res.status(500).json({ message: err });
       }
-      res
-        .status(200)
-        .json({
-          message: "User adde succesfully",
-          name,
-          email,
-          id: this.lastID,
-        });
+      res.status(200).json({
+        message: "User adde succesfully",
+        name,
+        email,
+        id: this.lastID,
+      });
     };
+});
+
+router.patch("/:id", (req, res) => {
+  const id = req.params.id;
+  const { name, email } = req.body;
+
+  db.run(
+    `UPDATE users SET name = ?, email = ? WHERE id = ?`,
+    [name, email, id],
+    (err) => {
+      if (err) {
+        return res.status(500).json({ message: err });
+      }
+      if (this.changer > 0) {
+        res
+          .status(200)
+          .json({ message: "User updated successfully", id, name, email });
+      } else {
+        res.status(404).json({ message: "User not found" });
+      }
+    }
+  );
 });
 
 export default router;
