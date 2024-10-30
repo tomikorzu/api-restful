4e7969f2a1dc8f3a5d7974c6fb8a96d349ea880c
tomikorzu <tomykorzu@icloud.com>
2024-10-30 12:19:15 -0300

    feat(logger.js): add the three functions of logger

--- express-router/index.js
+++ express-router/index.js
@@ -9,6 +9,13 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(logger);
+
+app.use("/users", userRouter);
+
+app.get("/protected", authenticate, (req, res) => {
+  res.status(200).json({ message: "Access allowed" });
+});
 
 app.use((err, req, res, next) => {
   console.error(err);
