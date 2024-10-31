b29d7a9985f07da9b97d8b72df4606f2df787d8f
tomikorzu <tomykorzu@icloud.com>
2024-10-31 12:26:22 -0300

    feat(register)

--- jwt-exercise/server/auth/register.js
+++ jwt-exercise/server/auth/register.js
@@ -1,6 +1,51 @@
 import db from "../config/users-db.js";
 import bcrypt from "bcrypt";
 
-const register = (req, res) => {};
+import roles from "../constants/roles.json";
+
+const register = (req, res) => {
+  const { username, email, password, fullname } = req.body;
+
+  if ((!username, !email, !password, !fullname)) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
+
+  db.get(
+    "SELECT * FROM users WHERE email = ? or username = ?",
+    [email, username],
+    (err, existingUser) => {
+      if (err) {
+        return res.status(500).json({ error: "Database error" });
+      }
+
+      if (existingUser) {
+        return res
+          .status(409)
+          .json({ error: "Email or username are already in use" });
+      }
+
+      const hashedPassword = bcrypt.hashSync(password, 10);
+
+      db.run(
+        "INSERT INTO users (username, email, password, fullname, role) VALUES (?, ?, ?, ?, ?)",
+        [username, email, hashedPassword, fullname, roles.employee],
+        (err) => {
+          if (err) {
+            return res.status(500).json({ error: "Error registering user" });
+          }
+
+          const token = jwt.sign(
+            { id: lastID, role: roles.employee },
+            secretKey
+          );
+
+          res
+            .status(201)
+            .json({ message: "User registered successfully", token });
+        }
+      );
+    }
+  );
+};
 
 export default register;
